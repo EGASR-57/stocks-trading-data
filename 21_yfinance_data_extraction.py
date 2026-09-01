6346ff2513f90663f21eb39b58c5f0ab3f1edda9
Eduardo González Agüero <edu.gonza.agu3@gmail.com>
2026-09-01 13:55:49 +0200

    A bit more, how to handle errors in calls?

--- 21_yfinance_data_extraction.py
+++ 21_yfinance_data_extraction.py
@@ -15,9 +15,14 @@ d = d.query('COUNTRY == "USA"')
 test = d['#SYM'].iloc[:100].to_list()
 # print(test)
 ticks = yf.Tickers(test)
-print(ticks.tickers)
-for t in ticks.tickers:
-    print(ticks.tickers[t].info)
 
 # -- We need decent code to detect those for which we may have info
-# -- we are using yfinance because in machine Bet we haven't TWS
+# -- we are using yfinance because in machine Bet we haven't TWS connection
+
+# for i in range(10):
+#     print(ticks.tickers[test[i]].info)
+
+# test[-1] registered; 0 is not. 
+p = ticks.tickers[test[-1]].info
+print(type(p))
+print(p.keys())

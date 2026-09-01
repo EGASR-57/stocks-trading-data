dbf7aade4bcc20429ba03929c726641865481c53
Eduardo González Agüero <edu.gonza.agu3@gmail.com>
2026-09-01 17:26:21 +0200

    Setup for large-scale query of yfinance

--- 21_yfinance_data_extraction.py
+++ 21_yfinance_data_extraction.py
@@ -22,17 +22,24 @@ test = d['#SYM'].iloc[:100].to_list()
 
 ticks = yf.Tickers(test)
 CATCH_ERROR = {'trailingPegRatio': None} # Determined experimentally :)
-
+TEST = False
 # print(ticks.tickers) # Dictionary symbol (str): Ticker (object)
 
-yexists = []
-yfails = []
-for i in ticks.tickers:
-    ticker = ticks.tickers[i]
-    info = ticker.info
-    if info == CATCH_ERROR:
-            yfails.append(i)
-    else:
-          yexists.append(i)
-          
-print(f"Total tickers: {len(test)}; {len(yexists)} exist in the universe of tickers of yfinance, {len(yfails)} don't")
+if TEST:
+    yexists = []
+    yfails = []
+    for i in ticks.tickers:
+        ticker = ticks.tickers[i]
+        info = ticker.info
+        if info == CATCH_ERROR:
+                yfails.append(i)
+        else:
+            yexists.append(i)
+
+    print(f"Total tickers: {len(test)}; {len(yexists)} exist in the universe of tickers of yfinance, {len(yfails)} don't")
+else:
+    p = pd.read_csv('csv/ftp_tickers.csv')
+    p['YF'] = None
+    print(p.head())
+    # Read subset of YF that are not T/F and query the API, update result as indicated
+    # Do it in batches of countries.
